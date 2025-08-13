703f577b0e8830f4a48bf1b4f0b6ea89777b655f
trungdam205 <trungdam205@gmail.com>
2025-08-13 20:43:52 +0700

    dang

---
+++ src/main/java/vn/codegym/cs_module3/HelloApplication.java
@@ -0,0 +1,9 @@
+package vn.codegym.cs_module3;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
